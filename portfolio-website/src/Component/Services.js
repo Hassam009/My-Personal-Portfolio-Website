95bb6ef7c20ef34856ca2e4844e9e48fcf54edcc
Hassam Amer <hasideveloper@gmail.com>
2024-01-21 23:58:17 +0500

    Added some Content

--- portfolio-website/src/Component/Services.js
+++ portfolio-website/src/Component/Services.js
@@ -1,18 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Services = () => {
   return (
- <>
- <div class="card" style={{width: "18rem"}}>
-  <img src="Images/B2.jpg" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
- </>
-  )
-}
+    <>
+      <div className="card mx-auto" style={{ width: '18rem' }}>
+        <img src="Images/B2.jpg" className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's content.
+          </p>
+          <Link to="#" className="btn btn-primary">
+            Go somewhere
+          </Link>
+        </div>
+      </div>
+      <div id="carouselExampleIndicators" className="carousel slide mx-auto" style={{ width: '80%' }} data-bs-ride="carousel">
+        <div className="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="Images/B1.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="Images/B5.jpg" className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="Images/B4.jpg" className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default Services
+export default Services;
