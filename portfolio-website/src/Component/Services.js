818b1e0151be325f723e928347ddd0550cfdda34
Hassam Amer <hasideveloper@gmail.com>
2024-01-23 00:21:21 +0500

    Again props applied

--- portfolio-website/src/Component/Services.js
+++ portfolio-website/src/Component/Services.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ServicesCards from '../Cards/ServicesCards';
 
 const Services = (props) => {
-
   return (
-    <>
-      <ServicesCards Title="Card 1"  Description="I am content of Card 1"></ServicesCards>
-      <ServicesCards Title="Card 2"  Description="I am content of Card 2"></ServicesCards>
-      <ServicesCards Title="Card 3" Description="I am content of Card 3"></ServicesCards>
- 
-    </>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-3">
+          <ServicesCards Title="Card 1" Description="I am content of Card 1" src="Images/B1.jpg"/>
+        </div>
+        <div className="col-md-3">
+          <ServicesCards Title="Card 2" Description="I am content of Card 2" src="Images/B2.jpg" />
+        </div>
+        <div className="col-md-3">
+          <ServicesCards Title="Card 3" Description="I am content of Card 3"  src="Images/B5.jpg"/>
+        </div>
+        <div className="col-md-3">
+          <ServicesCards Title="Card 3" Description="I am content of Card 3"  src="Images/B4.jpg"/>
+        </div>
+      </div>
+    </div>
   );
 };
 
