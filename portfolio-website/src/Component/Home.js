7f58738f1281f568d2b7f552f02198adf60e45fb
Hassam Amer <hasideveloper@gmail.com>
2024-01-24 23:59:02 +0500

    Some Changes

--- portfolio-website/src/Component/Home.js
+++ portfolio-website/src/Component/Home.js
@@ -1,68 +1,33 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Home = () => {
-
-const Wrapper=styled.section`
-.heroSection{
-  background-image: url('Images/B2.jpg');
-}
-.text-center{
-  color:White
-}
-`
+  const Wrapper = styled.section`
+    .heroSection {
+      background-image: url("Images/B2.jpg");
+    }
+    .text-center {
+      color: White;
+    }
+  `;
 
   return (
     <>
-    <Wrapper>
-    <div className="heroSection">
-      <h1 className="text-center">I am Home</h1>
-      <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
-        <div className="col mb-4">
-          <div className="card" style={{ maxWidth: '18rem' }}>
-            <img src="/Images/1.jpg" className="card-img-top img-fluid" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-              </p>
-            </div>
-          </div>
+      <Wrapper>
+        <div className="heroSection">
+          <h1 className="text-center">I am Home</h1>
+          <div class="container">
+  <div class="row">
+    <div class="col-md-6">
+      Column
+    </div>
+    <div class="col-md-6">
+      Column
+    </div>
+  </div>
+</div>
         </div>
-        <div className="col mb-4">
-          <div className="card" style={{ maxWidth: '18rem' }}>
-            <img src="/Images/1.jpg" className="card-img-top img-fluid" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="col mb-4">
-          <div className="card" style={{ maxWidth: '18rem' }}>
-            <img src="/Images/1.jpg" className="card-img-top img-fluid" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            </div>
-          </div>
-        </div>
-        <div className="col mb-4">
-          <div className="card" style={{ maxWidth: '18rem' }}>
-            <img src="/Images/1.jpg" className="card-img-top img-fluid" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      </div>
       </Wrapper>
     </>
   );
