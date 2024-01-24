7220dfc7903a59e1b1b05daf2adce51a6981a9ea
Hassam Amer <hasideveloper@gmail.com>
2024-01-24 23:52:42 +0500

    Styled Component Applied

--- portfolio-website/src/Component/Home.js
+++ portfolio-website/src/Component/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 const Home = () => {
+
+const Wrapper=styled.section`
+.heroSection{
+  Background-color:black;
+}
+`
+
   return (
     <>
+    <Wrapper>
+    <div className="heroSection">
       <h1 className="text-center">I am Home</h1>
-
       <div className="row row-cols-1 row-cols-md-2 g-4 justify-content-center">
         <div className="col mb-4">
           <div className="card" style={{ maxWidth: '18rem' }}>
@@ -49,6 +59,8 @@ const Home = () => {
           </div>
         </div>
       </div>
+      </div>
+      </Wrapper>
     </>
   );
 };
