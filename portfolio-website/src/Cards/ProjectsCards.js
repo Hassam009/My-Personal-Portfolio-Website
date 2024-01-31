f7f31663493c01ef99348dc41b74e90544138189
Hassam Amer <hasideveloper@gmail.com>
2024-01-31 13:19:31 +0500

    Done Filtration in Project Section

---
+++ portfolio-website/src/Cards/ProjectsCards.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProjectsCards = ({ selectCategory }) => {
+  const project = [
+    { id: 1, category: "Html", image: "/Images/B4.jpg" /* other properties */ },
+    { id: 2, category: "CSS", image: "/Images/B5.jpg" /* other properties */ },
+    {
+      id: 3, category: "React",image: "/Images/B3.jpg" /* other properties */},
+    // Add more projects as needed
+  ];
+
+  const filteredProject = project.filter((project) => {
+    return selectCategory ? project.category === selectCategory : true;
+  });
+
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="row">
+        {filteredProject.map((project) => (
+          <div className="col-md-4" key={project.id}>
+            <div className="card mx-2" style={{ width: "18rem" }}>
+              <img src={project.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{project.category}</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <Link to="#" className="btn btn-primary stretched-link">
+                  Go somewhere
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsCards;
--- portfolio-website/src/Component/Projects.js
+++ portfolio-website/src/Component/Projects.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react';
+import ProjectsCards from '../Cards/ProjectsCards';
 
 const Projects = () => {
-  return (
-   <>
-   <div className="Project">
-    <h1>I am Project Section</h1>
-   </div>
-   </>
-  )
+const [selectCategory, setSelectCategory]=useState();
+
+const handleCategoryClick=(category)=>{
+    setSelectCategory(category);
+}
+const handleCategoryClick2=(category)=>{
+    setSelectCategory();
 }
 
-export default Projects
+  return (
+    <>
+      <div className="Project d-flex flex-column align-items-center justify-content-center">
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+          <button type="button"
+          onClick={()=>handleCategoryClick2('All')}
+          class="btn btn-danger">
+            All
+          </button>
+          <button type="button"
+          onClick={()=>handleCategoryClick('Html')}
+          class="btn btn-danger">
+            Html
+          </button>
+          <button type="button"
+            onClick={()=>handleCategoryClick('CSS')}
+          class="btn btn-warning">
+            CSS
+          </button>
+          <button type="button" 
+            onClick={()=>handleCategoryClick('React')}
+            class="btn btn-success">
+        
+            React
+          </button>
+        </div>
+        <ProjectsCards  selectCategory={selectCategory}/>
+      </div>
+    </>
+  );
+};
+
+export default Projects;
